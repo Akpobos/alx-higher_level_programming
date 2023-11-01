03a2c0039b358ae6687bb4c62c9557ff28466a4a
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-01 02:08:05 -0700

    l

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if number < 0:
-    last_digit = last_digit * -1
-string_build = f"Last digit of {number:d} is {last_digit:d} "
-if last_digit > 5:
-    string_build = string_build + "and is greater than 5"
-elif last_digit == 0:
-    string_build = string_build + "and is 0"
-elif last_digit < 6 and last_digit != 0:
-    string_build = string_build + "and is less then 6 and not 0"
-print(string_build)
+if type(number) is int:
+    last_digit = str(number)[len(str(number)) - 1]
+    last = int(last_digit)
+    if number < 0:
+        last *= -1
+    if last > 5:
+        print(f"Last digit of {number} is {last} and is greater than 5")
+    elif last < 6 and last != 0:
+        print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+    elif last == 0:
+        print(f"Last digit of {number} is {last} and is 0")
+else:
+    raise TypeError("Only integers are allowed")
