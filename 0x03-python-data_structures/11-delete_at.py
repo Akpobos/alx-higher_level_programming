ebbefc0462fa127fe3a4b4253c8015941a9d76c8
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-06 00:26:58 -0800

    r

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,6 @@ def delete_at(my_list=[], idx=0):
 
     if idx < 0 or idx > (length - 1):
         return (my_list)
-    
+
     del my_list[idx:idx + 1]
-    
-    return (my_list)  
+    return (my_list)
