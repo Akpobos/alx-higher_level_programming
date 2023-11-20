8092dc6d40fdd8535ceeaeb13dc5777690a6d3f0
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-20 14:56:31 -0800

    ff

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """prints integer.
+
+    Args:
+        value (any): can be any type (integer, string, etc.)
+
+    Return:
+        A boolean
+    """
+    try:
+        print("{:d}", value)
+        return (True)
+    except:
+        return (False)
