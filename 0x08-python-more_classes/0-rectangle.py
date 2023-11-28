a1d4766888fb423675ac1d208e52af169fca3593
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-28 02:48:15 -0800

    ff

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""empty class Rectangle that defines a rectangle"""
+""" empty class Rectangle that defines a rectangle
+"""
+
 
 class Rectangle:
-    """Class that defines a Rectangle"""
+    """ class rectangle"""
     pass
