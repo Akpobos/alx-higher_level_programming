0fba4630063db282e19a1a88f0beb282ee84e3e8
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 08:05:40 -0800

    tt

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Geometry class"""
+
+
+class BaseGeometry:
+    """The Base class for a Geometry"""
+    
+    def area(self):
+        """Computes the area of the geometry
+        Raises:
+            Exception: For area not implemented
+        """
+        raise Exception("area() is not implemented")
