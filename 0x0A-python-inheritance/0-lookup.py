9e1a84666e9c77479892ab8abc3955bfc715b567
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 07:12:40 -0800

    hh

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,7 +2,8 @@
 """Module containing lookup"""
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object
+    """function that returns the list of available attributes
+    and methods of an object
     Args:
         obj (object): The object
     Return: A list of attributes
