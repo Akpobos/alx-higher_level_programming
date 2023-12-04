bf8fb38591a7590fc1fa21d2f598fdba2e160d4f
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 07:08:59 -0800

    ff

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing lookup"""
+
+def lookup(obj):
+    """function that returns the list of available attributes and methods of an object
+    Args:
+        obj (object): The object
+    Return: A list of attributes
+    """
+    return (dit(obj))
