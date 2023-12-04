0f4557c5b97e6e39f3c97d5233eed1a9f9c4341c
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 07:45:29 -0800

    tt

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module defining instance check"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that checks the type of classe"""
+    return True if isinstance(obj, a_class) else False
