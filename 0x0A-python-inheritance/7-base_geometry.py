56bd698767a9849d6506fabc9902a530708ca0d7
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 08:26:20 -0800

    tr

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,20 +4,22 @@
 
 class BaseGeometry:
     """The Base class for a Geometry"""
+
     def area(self):
         """Computes the area of the geometry
+
         Raises:
             Exception: For area not implemented
         """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validates a parameter as an integer
 
         Args:
             name (str): name of the value
             value (int): The value
-        
+
         Raises:
             TypeError: if value is not an integer
             ValueError: if value is <= 0
