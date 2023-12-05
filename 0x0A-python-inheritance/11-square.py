a9bdc206c0319b031c0ffe7e17532b187c554cf3
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-04 20:45:52 -0800

    ty

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,3 +17,7 @@ class Square(Rectangle):
     def area(self):
         """Returns the area of the square insatnce"""
         return self.__size ** 2
+
+    def __str__(self):
+        """Defines string representation"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
