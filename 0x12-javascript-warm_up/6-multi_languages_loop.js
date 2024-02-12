f70d0378186f3afcc3ad539486e090cf7d55c3b6
Emmanuel Alabi <130797110+Akpobos@users.noreply.github.com>
2024-02-12 12:37:03 +0000

    ee

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const phrase of arr) {
-    console.log(phrase);
-}
+arr.forEach((phrase) => console.log(phrase));
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
-    return console.log('Missing number of occurrences');
-}
-for (let i = parseInt(process.argv[2]); i > 0; i--) {
+const x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
+  }
 }
